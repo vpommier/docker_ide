b47e61b853ef9748a5dff88c737d7e6cc9b028f5
Vincent POMMIER <vincent.pommier@epsi.fr>
2015-01-19 23:06:12 +0100

    Ajouts: - Image IDE Vim (base) - Layers plugins Vim Go - Layers langage Go - Layers SDK Go appEngine

---
+++ build_goappengine.sh
@@ -0,0 +1,3 @@
+docker build -t opencloudlabs/ide:0.1 ./ide/ \
+&& docker build -t opencloudlabs/ide_plugin_go:0.1 ./ide_plugin_go \
+&& docker build -t opencloudlabs/ide_lang_goappengine:1.9.17 ./ide_lang_goappengine
