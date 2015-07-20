20a596a59cf27c22cf438ae1d4cbea45a0f34e56
Vincent POMMIER <vincent.pommier@epsi.fr>
2015-07-20 15:16:44 +0200

    Refactor

--- build_backup-repos.sh
+++
@@ -1,4 +0,0 @@
-docker build -t opencloudlabs/ide:latest ./ide/ \
-	&& docker build -t opencloudlabs/ide_plugin_go:latest ./ide_plugin_go/ \
-	&& docker build -t opencloudlabs/ide_lang_go:1.4.2 ./ide_lang_go/ \
-	&& docker build -t opencloudlabs/ide_backup-repos:0.0.1 ./ide_backup-repos/
--- build_goappengine.sh
+++
@@ -1,3 +0,0 @@
-docker build -t opencloudlabs/ide:latest ./ide/ \
-	&& docker build -t opencloudlabs/ide_plugin_go:latest ./ide_plugin_go/ \
-	&& docker build -t opencloudlabs/ide_lang_goappengine:1.9.23 ./ide_lang_goappengine/
